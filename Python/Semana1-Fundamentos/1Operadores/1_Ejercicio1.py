d92821449c4fcc46961cfd34643f4d8906c48f07
CyberNavX <gerbergarciasalas52@gmail.com>
2025-09-17 16:58:27 -0600

    Avance del Semana1-1Ope-1_Eje

---
+++ Python/Semana1-Fundamentos/1Operadores/1_Ejercicio1.py
@@ -0,0 +1,39 @@
+#    Semana1-FUndamentos ### 1OPeradores #### 1_Ejercicio1.py
+
+#----------Operaciones con operadores aritmeticos--------
+#
+#   Los operadores aritmeticos son los signos que conocemos en las operaciones basicas de las matematicas.
+#
+#   Python realiza calculos basicos con los operacores aritmeticos. El interprete evalua estas expresiones 
+#   siguiendoun orden de precedencia especifico para obtener el resultado.
+ 
+print("### Calculadora con operadores aritmeticos ###\n")
+
+
+numero1 = float(input("Ingrese el primer numero: "))            #   En la variable [numero1] se le pide al usuario un valor incluso el valor tenga punto decimal
+numero2 = float(input("Ingrese el segundo numero: "))           #   En la variable [numero2] se le pide al usuario un valor incluso el valor tenga punto decimal
+
+#Suma
+print("Operacion suma: ", numero1 + numero2)                    #   En el print identificamos que se esta haciendo una suma y el resultado esta concatenado en la misma linea con las variables [numero1 + numero2]
+
+#Resta
+print("Operacion resta: ", numero1 - numero2)                   #   En el print identificamos que se esta haciendo una resta y el resultado esta concatenado en la misma linea con las variables [numero1 - numero2]
+
+#Multiplicacion
+print("Operacion producto: ", numero1 * numero2)                #   En el print identificamos que se esta haciendo un producto y el resultado esta concatenado en la misma linea con las variables [numero1 * numero2]
+
+#Division
+print("Operacion division: ", numero1 / numero2)                #   En el print identificamos que se esta haciendo una division y el resultado esta concatenado en la misma linea con las variables [numero1 / numero2]
+
+#Division entera
+print("Operacion division entera: ", numero1 // numero2)        #   En el print identificamos que se esta haciendo una division entera y el resultado esta concatenado en la misma linea con las variables [numero1 // numero2]
+
+#Exponente
+print("Operacion exponente: ", numero1 ** numero2)              #   En el print identificamos que se esta haciendo un exponente y el resultado esta concatenado en la misma linea con las variables [numero1 ** numero2]
+
+#Modulo
+print("Operacion modulo: ", numero1 % numero2)                  #   En el print identificamos que se esta haciendo un modulo y el resultado esta concatenado en la misma linea con las variables [numero1 % numero2]
+
+
+
+
